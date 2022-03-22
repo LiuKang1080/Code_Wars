ec1cc6ee7fde1a321a87cbc46f04fd2b2a03e575
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-03-22 18:01:38 -0400

    Added Positive_Sum.js

---
+++ JavaScript/8_Kyu/Positive_Sum.js
@@ -0,0 +1,24 @@
+// Sum of positive integers from an array
+
+/*
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+*/
+
+
+function positive_sum(arr) {
+    let sum = 0;
+
+    // loop through the array
+    arr.forEach( (element) => {
+        // check to see if the ele is positive
+        if (element > 0) {
+            sum += element;
+        }
+    });
+
+    return sum;
+}
