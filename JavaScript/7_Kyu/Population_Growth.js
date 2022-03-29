69e2760c6c749965bef64d68a981af90e9c24842
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-03-29 17:10:18 -0400

    Added Population_Growth.js

---
+++ JavaScript/7_Kyu/Population_Growth.js
@@ -0,0 +1,64 @@
+// Growth of a population
+
+/*
+In a small town the population is p0 = 1000 at the beginning of a year. The population regularly increases by 2 percent per year
+and moreover 50 new inhabitants per year come to live in the town. How many years does the town need to see its population 
+greater or equal to p = 1200 inhabitants?
+
+At the end of the first year there will be: 
+1000 + 1000 * 0.02 + 50 => 1070 inhabitants
+
+At the end of the 2nd year there will be: 
+1070 + 1070 * 0.02 + 50 => 1141 inhabitants (** number of inhabitants is an integer **)
+
+At the end of the 3rd year there will be:
+1141 + 1141 * 0.02 + 50 => 1213
+
+It will need 3 entire years.
+(1000, 2, 50, 1200) -> 3
+
+More generally given parameters:
+p0, percent, aug (inhabitants coming or leaving each year), p (population to surpass)
+the function nb_year should return n number of entire years needed to get a population greater or equal to p.
+aug is an integer, percent a positive or null floating number, p0 and p are positive integers (> 0)
+
+Examples:
+nb_year(1500, 5, 100, 5000) -> 15
+nb_year(1500000, 2.5, 10000, 2000000) -> 10
+
+Note:
+Don't forget to convert the percent parameter as a percentage in the body of your function: if the parameter percent is 2 you 
+have to convert it to 0.02.
+*/
+
+
+function nbYear(p0, percent, aug, p) {
+    /*
+        - p0 = The initial starting number
+        - percent = The percent of growth
+        - aug = An integer, the flat number that is added each year
+        - p = The population target to surpass
+    */
+
+    // the general formula is: p0 + (p0 + percent) + aug
+    // this will then become the new total on the next iteration
+    
+    // declare variables
+    let years = 1;
+    let total = Math.floor(p0 + (p0 * (percent / 100)) + aug);
+    // WE MUST USE Math.floor() NOT Math.round()! It does not makes senese to have 1.8 number of people, you either have 1 or 2
+
+    // we will use a while loop, while total is less than or equal to p
+    while (total < p) {
+        // on each iteration use and update the general formula
+        total = Math.floor(total + (total * (percent / 100)) + aug);
+
+        // increment the number of years
+        years++;
+    }
+
+    return years;
+}
+
+
+console.log(nbYear(1000, 2, 50, 1200));
