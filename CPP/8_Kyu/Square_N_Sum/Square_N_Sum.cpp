21ab6cfdb10063a340147530266bd466f98f5b39
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-04-05 22:56:23 -0400

    Added more extensions to .gitignore. Added 8_Kyu Solution. Added the Project Square_N_Sum, and the .cpp file.

--- CPP/8_Kyu/Square_N_Sum/Square_N_Sum.cpp
+++ CPP/8_Kyu/Square_N_Sum/Square_N_Sum.cpp
@@ -10,11 +10,11 @@ For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 #include <vector>
 
 
-int square_sum(const std::vector<int> &numbers) {
+int square_sum(const std::vector<int>& numbers) {
     // iterate through the vector, on each iteration square the current element, add result to the total
     long long int total = 0;
 
-    for (int i: numbers) {
+    for (long long int i : numbers) {
         total += (i * i);
     }
 
@@ -23,7 +23,7 @@ int square_sum(const std::vector<int> &numbers) {
 
 
 int main() {
-    std::cout << square_sum({0, 3, 4, 5});
+    std::cout << square_sum({ 0, 3, 4, 5 }) << "\n";
 
     return 0;
 }
