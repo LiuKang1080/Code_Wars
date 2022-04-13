8e34442f188435f40957ba301a7ce9ef9c3373a0
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-04-13 18:28:22 -0400

    Added The_Return.js

---
+++ JavaScript/8_Kyu/The_Return.js
@@ -0,0 +1,40 @@
+// The Return
+
+/*
+Make multiple functions that will return the sum, difference, modulus, product, quotient, and the exponent respectively.
+
+Please use the following function names:
+addition = add
+multiply = multiply
+division = divide (both integer and float divisions are accepted)
+modulus = mod
+exponential = exponent
+subtraction = subt
+
+Note: All math operations will be: a (operation) b
+*/
+
+
+function add(x, y) {
+    return x + y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function mod(x, y) {
+    return x % y;
+}
+
+function exponent(x, y) {
+    return Math.pow(x, y);
+}
+
+function subt(x, y) {
+    return x - y;
+}
