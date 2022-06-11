23d7740150df08100d609351717546cd37493cc5
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-06-10 21:04:56 -0400

    Added more comments to reversed_strings.js

--- JavaScript/8_Kyu/reversed_strings.js
+++ JavaScript/8_Kyu/reversed_strings.js
@@ -12,6 +12,14 @@ function solution(str) {
     /*
     Takes the input string, and then returns the string reversed.
         - We will use the built-in functions .split(), .reverse(), and .join() to perform this algorithm.
+
+    Parameters:
+    ----------
+    str[string] : The string that is passed in, that will be reversed.
+    
+    Return
+    ----------
+    [string] : Implicitly return the reversed string. 
     */
 
     return str.split("").reverse().join("");
