36644c913b4b65cc584b3aaec70bea983d721a75
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-08-13 17:02:09 -0400

    Updated is_string_uppercase.js. Added is_string_uppercase.py.

--- Python/8_Kyu/is_string_uppercase.py
+++ Python/8_Kyu/is_string_uppercase.py
@@ -1,6 +1,6 @@
-// Is the string upper case?
+# Is the string upper case?
 
-/*
+"""
 Is the string uppercase?
 Task: Create a method to see whether the string is ALL CAPS.
 
@@ -15,11 +15,11 @@ Examples (input -> output):
 
 In this Kata, a string is said to be in ALL CAPS whenever it does not contain any lowercase letter so any string containing no 
 letters at all is trivially considered to be in ALL CAPS.
-*/
+"""
 
 
-function is_uppercase(str) {
-    /*
+def is_uppercase(inp):
+    """
     Returns True or False depending on if the string provided is completely upper case or not.
     Convert str fully into uppercase and store result into new variable called upper_str.
     If upper_str is equal to string, then we return true, else we will return false. [Explicit variable comparison]
@@ -33,17 +33,7 @@ function is_uppercase(str) {
     Return
     ----------
     true / false [boolean] : Explicitly return true or false
-    */
-
-    if (string === string.toUpperCase()) {
-        return true;
-    } else {
-        return false;
-    }
-}
-
-
-// extension of the String prototype object:
-String.prototype.isUpperCase = function() {
-    return (this == this.toUpperCase());
-}
+    """
+    
+    # we can perform the comparison and the return within a single line, we can use the .upper() string method:
+    return (inp == inp.upper())
