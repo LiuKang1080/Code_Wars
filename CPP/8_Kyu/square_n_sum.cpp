194e103595681bcbab8869e5f4bd792494fba791
Shivakumar Mahakali <mahakalim@yahoo.com>
2022-10-07 22:59:35 -0400

    Updated square_n_sum.cpp.

--- CPP/8_Kyu/square_n_sum.cpp
+++ CPP/8_Kyu/square_n_sum.cpp
@@ -11,9 +11,26 @@ For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
 
 
 int square_sum(const std::vector<int> &numbers) {
-    // iterate through the vector, on each iteration square the current element, add result to the total
+    /*
+	Loop through a given array, on each iteration square the current element, and add it to the total. Return the total.
+
+	Parameters
+	----------
+	numbers [const std::vector<int>] : The array. Pass by reference array.
+
+	Return
+	----------
+	total [long long int] : Explicity return the total. 
+	
+	Ex)
+		Parameter = [0, 3, 4, 5]
+		Return = 50
+	*/
+
+    // declare variables
     long long int total = 0;
 
+    // iterate through the vector, on each iteration square the current element, add result to the total
     for (long long int i : numbers) {
         total += (i * i);
     }
