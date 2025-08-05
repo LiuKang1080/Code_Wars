82d044b6d4a7c12f26c572f491a81d45a1b3c793
Shivakumar Mahakali <mahakalim@yahoo.com>
2025-08-05 03:59:43 -0400

    Added the Rust folder for Rust excersizes and programs.

---
+++ Rust/8_Kyu/hello_world_function/src/main.rs
@@ -0,0 +1,23 @@
+/*
+========== ========== ==========
+Hello World Function
+========== ========== ==========
+Description:
+
+Make a simple function called greet that returns the most-famous "hello world!".
+Style Points
+
+Sure, this is about as easy as it gets. But how clever can you be to create the most creative hello world you can think of? What
+is a "hello world" solution you would want to show your friends?
+*/
+
+
+fn greet() {
+    println!("Hello, world!");
+}
+
+
+fn main() {
+    // greet function
+    greet();    
+}
